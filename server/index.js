c247d32424d6a3974cbc09acaddc97a924ebbacb
BM086325 <Borys_Mohyla@epam.com>
2021-06-22 20:50:21 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use("/static", express.static("./public"));
 //connection to db
 mongoose.set("useFindAndModify", false);
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-    console.log("Connected to db!", err);
+    console.log("Connected to db!");
     app.listen(4000, () => console.log("Server Up and running"));
 });
 
